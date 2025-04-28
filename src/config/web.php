00cecd0870baaa56761d3645a85e7b255319d33b
Galla Karthikeyudu <108474772+Gkarthikgithub@users.noreply.github.com>
2025-04-28 21:32:29 +0530

    Update web.php

--- src/config/web.php
+++ src/config/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use yii\web\Application;
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
     'components' => [
         'request' => [
             'cookieValidationKey' => 'your-validation-key',
@@ -12,15 +14,40 @@ $config = [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => true,
         ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+
+        // ADD THIS i18n block
+        'i18n' => [
+            'translations' => [
+                'yii/bootstrap5' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@yii/bootstrap5/messages',
+                ],
+            ],
+        ],
+        // end i18n
     ],
-    'defaultRoute' => 'site/index',
+    'params' => $params,
 ];
 
 return $config;
