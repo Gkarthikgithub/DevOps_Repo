2a4253728a77b298cc87e784633b89e723ad991d
Galla Karthikeyudu <108474772+Gkarthikgithub@users.noreply.github.com>
2025-04-28 20:54:52 +0530

    Create index.php

---
+++ src/views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Yii2 Basic App';
+?>
+
+<div class="site-index">
+    <h1>Welcome to Yii2 Sample App!</h1>
+    <p>If you see this page, your Yii2 app is deployed correctly via Docker Swarm!</p>
+</div>
