009618903d4640e33bc8ef3bfa7e846eb5da822c
Galla Karthikeyudu <108474772+Gkarthikgithub@users.noreply.github.com>
2025-04-28 20:57:01 +0530

    Create main.php

---
+++ src/views/layouts/main.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $this->title ?></title>
+</head>
+<body>
+    <div class="wrap">
+        <?= $content ?>
+    </div>
+</body>
+</html>
