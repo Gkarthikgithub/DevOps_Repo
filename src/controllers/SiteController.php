78de057679a57f11093d27b4625108ca6204d630
Galla Karthikeyudu <108474772+Gkarthikgithub@users.noreply.github.com>
2025-04-28 20:54:00 +0530

    Create SiteController.php

---
+++ src/controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionError()
+    {
+        return $this->render('error');
+    }
+}
